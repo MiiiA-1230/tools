78904884160e337cc5a4a0662966cbc25593680c
MiiiA-1230 <llx4076g@gmail.com>
2024-06-06 11:50:01 +0800

    优化了zh_user.sh的运行代码，添加了高权限的创建与删除

--- user/zh_user.sh
+++ user/zh_user.sh
@@ -14,37 +14,62 @@ cat <<EOF
   目前可提供的功能有：
   1  创建用户
   2  删除用户
+  3  创建高级权限用户
+  4  对高级权限用户进行降权
 EOF
 
-read -p "你想要进行的操作是：" num
+read -p "你想要进行的操作是：" Num
 
-case $num in 
-1)
-  read -p "请输入你想创建的用户名称：" user_name
-  id $user_name &> /dev/null
-  if [ $? -eq 0 ]
-  then
-    addlog "创建用户失败，用户已存在"
-  else
-    useradd $user_name &> /dev/null
-    addlog "成功创建用户"
-    id $user_name
-  fi
-;;
-2)
-  read -p "请输入你想删除的用户名称：" user_name
-  id $user_name &> /dev/null
-  if [ $? -eq 0 ]
-  then
-    userdel -r $user_name
-    addlog "删除成功"
-  else
-    addlog "不存在该用户，无法删除"
-  fi
-;;
-*)
-  echo "输入错误，正在退回主菜单"
-;;
+case $Num in 
+  1)
+    read -p "请输入你想创建的用户名称：" User_Name
+    read -p "请输入你想创建的用户密码：" User_Password
+    id $User_Name &> /dev/null
+    if [ $? -eq 0 ]
+    then
+      addlog "创建用户失败，用户已存在"
+    else
+      useradd $User_Name &> /dev/null
+      echo $User_Password | passwd --stdin $User_Name
+      addlog "成功创建用户"
+      id $User_Name
+    fi
+  ;;
+  2)
+    read -p "请输入你想删除的用户名称：" User_Name
+    id $User_Name &> /dev/null
+    if [ $? -eq 0 ]
+    then
+      userdel -r $User_Name
+      addlog "删除成功"
+    else
+      addlog "不存在该用户，无法删除"
+    fi
+  ;;
+  3)
+    read -p "请输入你想创建的用户名称：" User_Name
+    read -p "请输入你想创建的用户密码：" User_Password
+    id $User_Name &> /dev/null
+    if [ $? -eq 0 ]
+    then
+      addlog "创建用户失败，用户已存在"
+    else
+      useradd $User_Name &> /dev/null
+      echo $User_Password | passwd --stdin $User_Name
+      echo "$User_Name ALL=(ALL) ALL" >> /etc/sudoers
+      addlog "成功创建高级权限用户"
+      id $User_Name
+    fi
+  ;;
+  4)
+    read -p "请输入你想降权的用户名称：" User_Name
+    id $User_Name &> /dev/null
+    cp /etc/sudoers /etc/sudoers.bak
+    sed -i "/^$User_Name ALL=(ALL) ALL/d" /etc/sudoers
+  ;;
+  *)
+    echo "输入错误，正在退回主菜单"
+  ;;
 esac
 
 sleep 1
